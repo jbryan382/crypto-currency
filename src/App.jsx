b1cbe3c4302b05ee1e2b391af325a46d51ebf55c
Jordan Bryan <jbryan382@gmail.com>
2021-02-25 02:06:56 -0600

    React Hooks Object Entries example

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export function App() {
-  return <div>Hello, World!</div>
+  const [currencyResponse, setCurrencyResponse] = useState({ rates: [] })
+  const [amount, setAmount] = useState(1)
+
+  useEffect(async () => {
+    const response = await axios.get(
+      `https://api.coingecko.com/api/v3/exchange_rates`
+    )
+    setCurrencyResponse(response.data)
+  }, [])
+
+  useEffect(() => {
+    console.log(currencyResponse)
+  }, [currencyResponse])
+
+  function sanitizeAmount() {
+    return amount
+  }
+
+  return (
+    <>
+      <h1> Crypto-Currency-Converter</h1>
+      <ul>
+        {Object.entries(currencyResponse.rates).map(
+          ([currencyCode, currencyDetails]) => {
+            return (
+              <li key={currencyCode}>
+                {currencyDetails.name} ({currencyCode}):
+                {(currencyDetails.value * amount).toFixed(2)}
+              </li>
+            )
+          }
+        )}
+      </ul>
+      <input
+        type="number"
+        onChange={(event) => setAmount(event.target.value)}
+        value={sanitizeAmount()}
+      />
+    </>
+  )
 }
